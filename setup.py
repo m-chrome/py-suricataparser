b3dcb997b8c6323fd447513378f069b48601a694
Michail Tsyganov <tsyganov.michail@yandex.ru>
2020-08-02 16:24:42 +0300

    Add github url

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     name="suricataparser",
     version=suricataparser.__version__,
     author="Michail Tsyganov",
+    url="https://github.com/m-chrome/py-suricataparser",
     description="Suricata rule parser",
     packages=["suricataparser"],
     python_requires=">=3.6"
